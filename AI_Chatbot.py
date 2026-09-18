bc372b2614cfd0c664c785dd795383ee7debaca9
Shayan <shayanskandari625@gmail.com>
2026-09-18 18:18:09 +0330

    Clean extra spaces from user name

--- AI_Chatbot.py
+++ AI_Chatbot.py
@@ -3,34 +3,70 @@ name = None
 while True:
     user_input = input("You: ").lower()
 
+    # ==========================
+    # Exit
+    # ==========================
+
     if user_input == "exit":
         break
+
+    # ==========================
+    # Hello Hi Hey
+    # ==========================
 
     elif user_input in ["hello", "hi", "hey"]:
         if name is not None:
             print(f"hello {name}, how can i help you?")
         else:
-            print("hello! how can i help you?")    
+            print("hello! how can i help you?")  
+
+    # ==========================
+    # Goodbye See you See ya
+    # ==========================          
 
 
     elif user_input in ["bye","goodbye","see you","see ya"]:
         print("Bot: Goodbye! See you later.") 
 
+    # ==========================
+    # How are you
+    # ==========================    
+
     elif user_input == "how are you":
         print("Bot: I'm doing great! Thanks for asking.")
+
+    # ==========================
+    # Thanks Thank you Thx
+    # ==========================      
 
     elif user_input in ["thanks","thank you","thx"]:
         print("Bot: You're welcome!")
 
+    # ==========================
+    # Good Morning
+    # ==========================     
+
     elif user_input == "good morning":
         print("Bot: Good morning! Have a great day!")
+
+    # ==========================
+    # Good night
+    # ==========================     
 
     elif user_input == "good night":
         print("Bot: Good night! Sleep well!")
 
+
+    # ==========================
+    # My name is . . . 
+    # ==========================    
+
     elif user_input.startswith("my name is "):
-        name = user_input.replace("my name is", "")
+        parts =  user_input.split()
+        name = parts[3].strip()
+
         print(f"Bot: Nice to meet you, {name}!")
+
 
     elif user_input == "what is my name?":
         if name is not None:
@@ -40,6 +76,10 @@ while True:
         else:
             print("Bot: i don't know your name yet.")    
 
+
+    # ==========================
+    # None
+    # ==========================
         
     else:
         print("Sorry, I dont understand that yet.")
